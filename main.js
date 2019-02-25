a1ca409a275e9c8a667c59d9427db7d41a8f3a0a
David Mora <David.mora@codeimmersives.com>
2019-02-25 12:46:18 -0500

    wrote initial java script code for title case

--- main.js
+++ main.js
@@ -2,12 +2,29 @@
 
 function init() {
     document.querySelector('#input')
-        .addEventListener('keydown', handleTyping);
+        .addEventListener('keyup', handleTyping);
 }
 
 function handleTyping(event) {
     let userInput = event.target.value;
-    let answer = userInput;
+    let answer = '';
+    let space = ' ';
+    let i = 0; 
+    // 1. As long as "i" = index is less than the userInput than...
+    // 2. target any value where itself minus one is equal to a "space"
+    //          a. OR if the index 'i' is equal to 0
+    // 4. if so take the input and input whos index fulfills requirements and make it uppercase
+    // 5. if statements are false then simply take input and input whos index doesnt fulfill requirements and make them lowercase
+    // 6. repeat
+
+    while (i < userInput.length) {
+        if (userInput[i - 1] === space || i === 0) {
+            answer = answer + userInput[i].toUpperCase();
+        } else {
+            answer = answer + userInput[i].toLowerCase();
+        }
+        i++
+    }
     
-    document.querySelector('#changeme').innerText = answer;
+    document.querySelector('#titlecase').innerText = answer;
 }
